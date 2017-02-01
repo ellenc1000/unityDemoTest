99f9c6f245e870ed0d9c9c83a125a79bc4946dd5
Unknown <ellenc1000@hotmail.com>
2017-02-01 14:36:27 +0000

    test commit

---
+++ Assets/cubemove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cubemove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public Transform from;
+	public Transform to;
+	public float speed = 0.1f;
+
+	// Update is called once per frame
+	void Update () {
+		float moveSpeed = speed * Time.deltaTime;
+		transform.rotation = Quaternion.Lerp(transform.rotation, to.rotation, moveSpeed);//current pos, end pos, movement
+		transform.position = Vector3.Lerp(transform.position, to.position, moveSpeed);
+	}
+}
